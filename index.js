a400ba6417588fa7bbf71f29c59d1f1eca5265f5
a-gomathi <65897231+a-gomathi@users.noreply.github.com>
2021-03-05 15:23:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const moment = require('moment')
-const timeStamp = moment()
-console.log(`Hello Everyone, Today is ${timeStamp}.\nHave a great day!`)
+// const user = 'gomathia'
+console.log("process.env.Docker username \t: " + process.env.Docker_USR);
+console.log("process.env.Docker password \t: " + process.env.Docker_PSW);
+// console.log("process.env.myPassword \t: " + process.env.myPassword);
+
+const options = {
+      method: 'GET',
+      uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository:urbancode/ucv-ext-jira:pull`,
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(process.env.Docker_USR + ':' + process.env.Docker_PSW).toString('base64')
+      }
+    }
+console.log("\n options with base64\t: " + JSON.stringify(options));
